69614c565fc08e2190e2c45e979d67754860f67c
Barnabás Jász (laptop) <jaszbarni123@gmail.com>
2024-10-16 11:41:24 +0200

    added some comments

--- src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
+++ src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
@@ -31,7 +31,7 @@ public class TorpedoStore {
 
   public boolean fire(int numberOfTorpedos){
     if(numberOfTorpedos < 1 || numberOfTorpedos > this.torpedoCount){
-      throw new IllegalArgumentException("numberOfTorpedos");
+      throw new IllegalArgumentException("numberOfTorpedos");  //throwing exception to indicate if the numberOfTorpedos value is not right 
     }
 
     boolean success = false;
